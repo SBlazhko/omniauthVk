f871951f622aa0de549dd2dd7cc04f66a358f620
as-sergey-b <sergey.b.as@agiliumlabs.com>
2016-12-09 07:01:17 +0200

    secret key

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 20161205001453) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "accounts", force: :cascade do |t|
     t.string   "uid"
     t.string   "provider"
