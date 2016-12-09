a30976a95978611c67e7613b30ceadb78ff6500a
as-sergey-b <sergey.b.as@agiliumlabs.com>
2016-12-09 15:06:39 +0200

    add mock

--- spec/features/omniauth_spec.rb
+++ spec/features/omniauth_spec.rb
@@ -2,17 +2,22 @@ require "rails_helper"
 require 'spec_helper'
 
 feature "Omniauth VK" do
-  scenario "Signing in " do
+  before(:each) do
+    OmniAuth.config.mock_auth[:vkontakte] = OmniAuth::AuthHash.new(
+      provider: 'vkontakte',
+      uid: '22222',
+      info: { email: 'Blaghco@ukr.net' }
+    )
     visit '/sessions/new'
     click_link 'VK'
+  end
 
+  scenario "Signing in " do
     expect(page).to have_content 'Blaghco@ukr.net'
     expect(page).to have_content 'Logout'
   end
 
   scenario "Signing out" do
-    visit '/sessions/new'
-    click_link 'VK'
     click_link 'Logout'
 
     expect(page).to have_content "VK"
